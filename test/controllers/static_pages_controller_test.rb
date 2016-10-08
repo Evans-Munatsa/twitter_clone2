d89c44c941c2b79308454fdf32911d4bc78924f1
Evans-Munatsa <gagamel.em@gmail.com>
2016-10-08 04:10:47 +0200

    added design and working routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
-	
+
   def setup
     @base_title = "Twitter clone"
   end
@@ -8,7 +8,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
@@ -21,5 +21,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :about
     assert_response :success
     assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
